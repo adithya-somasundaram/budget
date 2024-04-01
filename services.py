5395443041ed91cdc2c72f7b72f454aed1666acd
adithya-somasundaram <43027530+adithya-somasundaram@users.noreply.github.com>
2024-03-31 17:15:26 -0700

    Credit payments v1 (#6)
    * create credit payments table
    
    * credit

--- services.py
+++ services.py
@@ -1,5 +1,5 @@
 from app import db
-from models import Transaction, TransactionType
+from models import Transaction, TransactionType, CreditSource, CreditPayment
 from sqlalchemy.sql import func
 
 session = db.session
@@ -9,9 +9,18 @@ def create_new_transaction(amount_in_cents:int, type:TransactionType, descriptio
     try:
         session.add(new_transaction)
         session.commit()
-        print('Successfully create transaction of type ' + str(type) + ' and amount ' + str(amount_in_cents) + '\n' + description)
+        print('Successfully created transaction of type ' + str(type) + ' and amount ' + str(amount_in_cents) + '\n' + description)
     except:
         print('Could not create transaction of type ' + str(type) + ' and amount ' + str(amount_in_cents))
+
+def create_new_credit_payment(amount_in_cents:int, type:CreditSource, description):
+    new_payment = CreditPayment(amount_in_cents=amount_in_cents, credit_type=type, description=description)
+    try:
+        session.add(new_payment)
+        session.commit()
+        print('Successfully create credit payment for ' + str(type) + ' and amount ' + str(amount_in_cents) + '\n' + description)
+    except:
+        print('Could not create credit payment for ' + str(type) + ' and amount ' + str(amount_in_cents))
 
 def print_cents_in_dollars(amount):
     dollars = int(amount / 100)
@@ -22,10 +31,31 @@ def print_cents_in_dollars(amount):
     else:
         cents = str(cents)
 
-    return '$' + str(dollars) + '.' + cents
+    dollar_output = ''
+
+    while dollars > 0:
+        dollar_temp = dollars % 1000
+        dollars = int(dollars / 1000)
+        
+        if dollars > 0:
+            if dollar_temp < 10:
+                dollar_temp = '00' + str(dollar_temp)
+            elif dollar_temp < 100:
+                dollar_temp = '0' + str(dollar_temp)
+        
+        dollar_output = str(dollar_temp) + dollar_output
+        
+        if dollars > 0:
+            dollar_output = "," + dollar_output
+
+    return '$' + dollar_output + '.' + cents
 
 def get_summary():
     totals = session.query(Transaction.type, func.sum(Transaction.amount_in_cents).label('amount_in_cents')).group_by(Transaction.type).all()
 
+    grand_total = 0
     for total in totals:
+        grand_total += total.amount_in_cents
         print(str(total.type) + ': ' + print_cents_in_dollars(total.amount_in_cents))
+
+    print('TOTAL: ' + print_cents_in_dollars(grand_total))
