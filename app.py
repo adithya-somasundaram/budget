c5f2779c8d63812cd27466792d2151f2b0622cce
adithya-somasundaram <43027530+adithya-somasundaram@users.noreply.github.com>
2025-01-18 18:49:35 -0800

    Budget/formatting (#8)
    * formatting
    
    * comments

--- app.py
+++ app.py
@@ -2,13 +2,15 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///soma.db'
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///soma.db"
 db = SQLAlchemy(app)
 
-@app.route('/')
-def index():
-    return 'Connected'
 
-if __name__ == '__main__':
+@app.route("/")
+def index():
+    return "Connected"
+
+
+if __name__ == "__main__":
     app.app_context().push()
     app.run(debug=True, port=9876)
--- services.py
+++ services.py
@@ -1,64 +1,116 @@
-from app import db
-from models import Transaction, TransactionType, CreditSource, CreditPayment
 from sqlalchemy.sql import func
+
+from app import db
+from models import CreditPayment, CreditSource, Transaction, TransactionType
 
 session = db.session
 
-def create_new_transaction(amount_in_cents:int, type:TransactionType, description:str):
-    new_transaction = Transaction(amount_in_cents=-amount_in_cents, type=type, description=description)
+
+def create_new_transaction(
+    amount_in_cents: int, type: TransactionType, description: str
+):
+    """Creates a transaction of type type. By default, transactions are stored as negative."""
+    new_transaction = Transaction(
+        amount_in_cents=-amount_in_cents, type=type, description=description
+    )
     try:
         session.add(new_transaction)
         session.commit()
-        print('Successfully created transaction of type ' + str(type) + ' and amount ' + str(amount_in_cents) + '\n' + description)
+        print(
+            "Successfully created transaction of type "
+            + str(type)
+            + " and amount "
+            + str(amount_in_cents)
+            + "\n"
+            + description
+        )
     except:
-        print('Could not create transaction of type ' + str(type) + ' and amount ' + str(amount_in_cents))
+        print(
+            "Could not create transaction of type "
+            + str(type)
+            + " and amount "
+            + str(amount_in_cents)
+        )
 
-def create_new_credit_payment(amount_in_cents:int, type:CreditSource, description):
-    new_payment = CreditPayment(amount_in_cents=amount_in_cents, credit_type=type, description=description)
+
+def create_new_credit_payment(amount_in_cents: int, type: CreditSource, description):
+    """Creates a new credit payment."""
+    new_payment = CreditPayment(
+        amount_in_cents=amount_in_cents, credit_type=type, description=description
+    )
     try:
         session.add(new_payment)
         session.commit()
-        print('Successfully create credit payment for ' + str(type) + ' and amount ' + str(amount_in_cents) + '\n' + description)
+        print(
+            "Successfully create credit payment for "
+            + str(type)
+            + " and amount "
+            + str(amount_in_cents)
+            + "\n"
+            + description
+        )
     except:
-        print('Could not create credit payment for ' + str(type) + ' and amount ' + str(amount_in_cents))
+        print(
+            "Could not create credit payment for "
+            + str(type)
+            + " and amount "
+            + str(amount_in_cents)
+        )
 
-def print_cents_in_dollars(input):
-    amount = input
+
+def print_cents_in_dollars(value):
+    """Default printing function. Input value in cents"""
+    amount = value
     if amount < 0:
         amount = amount * -1
     dollars = int(amount / 100)
     cents = amount % 100
 
     if cents < 10:
-        cents = '0' + str(cents)
+        cents = "0" + str(cents)
     else:
         cents = str(cents)
 
-    dollar_output = ''
+    dollar_output = ""
 
     while dollars > 0:
         dollar_temp = dollars % 1000
         dollars = int(dollars / 1000)
-        
+
         if dollars > 0:
             if dollar_temp < 10:
-                dollar_temp = '00' + str(dollar_temp)
+                dollar_temp = "00" + str(dollar_temp)
             elif dollar_temp < 100:
-                dollar_temp = '0' + str(dollar_temp)
-        
+                dollar_temp = "0" + str(dollar_temp)
+
         dollar_output = str(dollar_temp) + dollar_output
-        
+
         if dollars > 0:
             dollar_output = "," + dollar_output
 
-    return ('-' if input < 0 else '') + '$' + (dollar_output if len(dollar_output) > 0 else '0') + '.' + cents
+    return (
+        ("-" if value < 0 else "")
+        + "$"
+        + (dollar_output if len(dollar_output) > 0 else "0")
+        + "."
+        + cents
+    )
+
 
 def get_summary():
-    totals = session.query(Transaction.type, func.sum(Transaction.amount_in_cents).label('amount_in_cents')).group_by(Transaction.type).all()
+    """Sums and returns all transactions by type. Also calculates total net value."""
+    totals = (
+        session.query(
+            Transaction.type,
+            func.sum(Transaction.amount_in_cents).label("amount_in_cents"),
+        )
+        .group_by(Transaction.type)
+        .all()
+    )
 
     grand_total = 0
     for total in totals:
         grand_total += total.amount_in_cents
-        print(str(total.type) + ': ' + print_cents_in_dollars(total.amount_in_cents))
+        print(str(total.type) + ": " + print_cents_in_dollars(total.amount_in_cents))
 
-    print('TOTAL: ' + print_cents_in_dollars(grand_total))
+    print("TOTAL: " + print_cents_in_dollars(grand_total))
