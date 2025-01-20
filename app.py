29393835b2abf2b2864be5b822a2dc2abd7796b2
Adithya <aditsom@gmail.com>
2025-01-19 20:52:41 -0800

    reorg

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///soma.db"
 db = SQLAlchemy(app)
+session = db.session
 
 
 @app.route("/")
---
+++ src/transactions/model.py
@@ -0,0 +1,27 @@
+import enum
+from datetime import datetime
+
+from sqlalchemy import Enum
+from sqlalchemy.sql.schema import Column
+from sqlalchemy.sql.sqltypes import DateTime, Integer, String
+
+from app import db
+from src.credit_payments.model import CreditSource
+
+
+class TransactionType(enum.Enum):
+    CREDIT = 1
+    DEBIT = 2
+    CASH = 3
+    CHECK = 4
+    VENMO = 5
+
+
+class Transaction(db.Model):
+    id = Column(Integer, primary_key=True)
+    amount_in_cents = Column(Integer, nullable=False)
+    type = Column(Enum(TransactionType), nullable=False)
+    credit_type = Column(Enum(CreditSource), nullable=True)
+    description = Column(String(200))
+    created_at = Column(DateTime, default=datetime.utcnow)
+    updated_at = Column(DateTime, default=datetime.utcnow)
