866835421da666d5805c1ca879719dabbbe202a0
Adithya <aditsom@gmail.com>
2025-01-25 12:07:53 -0800

    model base

---
+++ src/accounts/model.py
@@ -0,0 +1,31 @@
+from datetime import datetime
+
+import pytz
+from sqlalchemy import Enum
+from sqlalchemy.sql.schema import Column
+from sqlalchemy.sql.sqltypes import Boolean, DateTime, Integer, String
+
+from app import db
+from src.transactions.model import TransactionType
+
+timezone = pytz.timezone("America/Los_Angeles")
+
+
+class Accounts(db.Model):
+    id = Column(Integer, primary_key=True)
+    value_in_cents = Column(Integer, nullable=False)
+    type = Column(Enum(TransactionType), nullable=False)
+    name = Column(String, unique=True)
+    is_active = Column(Boolean, nullable=False)
+    created_at = Column(DateTime, default=datetime.now(timezone))
+    updated_at = Column(DateTime, default=datetime.now(timezone))
+
+
+class AccountRecords(db.Model):
+    id = Column(Integer, primary_key=True)
+    value_in_cents = Column(Integer, nullable=False)
+    type = Column(Enum(TransactionType), nullable=False)
+    name = Column(String, unique=True)
+    is_active = Column(Boolean, nullable=False)
+    created_at = Column(DateTime, default=datetime.now(timezone))
+    updated_at = Column(DateTime, default=datetime.now(timezone))
