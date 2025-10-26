fcb8eee56bc142547bbb6727662bd25b3e09ce55
Adithya <aditsom@gmail.com>
2025-10-26 16:40:02 -0700

    adjust import

--- scripts.py
+++ scripts.py
@@ -1,5 +1,6 @@
 from app import *
 from src.accounts.services import *
+from src.budget_categories.services import *
 from src.credit_payments.services import *
 from src.transactions.services import *
 
