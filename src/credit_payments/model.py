67e2074202b3d09d2dd268d2afc93da5642ff18e
Adithya <aditsom@gmail.com>
2025-10-27 21:35:34 -0700

    add DOT to models

--- src/credit_payments/model.py
+++ src/credit_payments/model.py
@@ -1,7 +1,7 @@
 from datetime import datetime
 
 import pytz
-from sqlalchemy import ForeignKey
+from sqlalchemy import Date, ForeignKey
 from sqlalchemy.sql.schema import Column
 from sqlalchemy.sql.sqltypes import DateTime, Integer, String
 
@@ -14,6 +14,7 @@ class CreditPayment(db.Model):
     id = Column(Integer, primary_key=True)
     amount_in_cents = Column(Integer, nullable=False)
     credit_account_id = Column(Integer, ForeignKey("account.id"), nullable=False)
+    date_of_transaction = Column(Date, nullable=False)
     description = Column(String(200))
     created_at = Column(DateTime, default=datetime.now(timezone))
     updated_at = Column(DateTime, default=datetime.now(timezone))
--- src/credit_payments/services.py
+++ src/credit_payments/services.py
@@ -1,4 +1,6 @@
-from datetime import date
+from datetime import date, datetime
+
+import pytz
 
 from src.accounts.model import Account
 from src.credit_payments.model import CreditPayment
@@ -7,7 +9,11 @@ from src.transactions.services import create_new_transaction
 
 
 def create_new_credit_payment(
-    session, amount_in_cents: int, account_name: str, description
+    session,
+    amount_in_cents: int,
+    account_name: str,
+    description: str = None,
+    date_of_transaction_str: str = None,
 ):
     """Creates a new credit payment."""
     credit_account: Account = (
@@ -22,10 +28,20 @@ def create_new_credit_payment(
                 f"Account of name {account_name} not found. Payment not processed"
             )
 
+        date_of_transaction = None
+        if not date_of_transaction_str:
+            pacific_timezone = pytz.timezone("America/Los_Angeles")
+            date_of_transaction = datetime.now(pacific_timezone).date()
+        else:
+            date_of_transaction = datetime.strptime(
+                date_of_transaction_str, "%Y-%m-%d"
+            ).date()
+
         new_payment = CreditPayment(
             amount_in_cents=amount_in_cents,
             credit_account_id=credit_account.id,
             description=description,
+            date_of_transaction=date_of_transaction,
         )
         today = date.today()
         transaction_message = f"Credit payment for {account_name} on {today}"
