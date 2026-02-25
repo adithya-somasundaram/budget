d7521b377d740a6fab01ca2570985c36185a77bb
Adithya <aditsom@gmail.com>
2026-02-24 16:51:46 -0800

    align

--- src/accounts/services.py
+++ src/accounts/services.py
@@ -127,14 +127,15 @@ def get_summary(session):
     )
     output = ""
     grand_total = 0
+
+    max_len = max(len(account.name) for account in accounts)
+
     for account in accounts:
         if account.type == AccountType.CREDIT:
             grand_total -= account.value_in_cents
         else:
             grand_total += account.value_in_cents
-        output += "{0:10} : {1}\n".format(
-            account.name, cents_to_dollars_str(account.value_in_cents)
-        )
+        output += f"{account.name:<{max_len}} : {cents_to_dollars_str(account.value_in_cents)}\n"
 
     return "{0:10}TOTAL: {1}".format(output, cents_to_dollars_str(grand_total))
 
