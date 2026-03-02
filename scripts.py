c3c0dea59ee418159463e0e1110ed5e63fb374b6
adithya-somasundaram <43027530+adithya-somasundaram@users.noreply.github.com>
2026-03-02 13:40:54 -0800

    Budget/readme cleanup (#36)
    * sync readme and scripts
    
    * add functions to readme

--- scripts.py
+++ scripts.py
@@ -1,11 +1,14 @@
 from app import *
 from src.accounts.services import (
+    adjust_account_value,
     bulk_create_accounts,
     create_new_account,
     deactivate_account,
+    print_summary,
     transfer,
 )
 from src.budget_categories.services import (
+    adjust_budget_category,
     create_budget_category,
     deactivate_budget_category,
 )
