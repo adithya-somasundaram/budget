7fc3b30750f3140414b64c7e7a87c963d5902ea8
Adithya <aditsom@gmail.com>
2026-04-28 21:17:47 -0700

    comments

--- src/accounts/services.py
+++ src/accounts/services.py
@@ -80,46 +80,51 @@ def transfer(session):
 
     account_mapping = get_all_accounts_mapping(session)
 
+    # User selects from account from list of active accounts
     print("Enter from account number: ")
     for i, account in account_mapping.items():
         print(f"\n({i}) {account.name}")
-
     from_account_name = input().strip()
     if from_account_name.lower() in exit_keys:
         return
     from_account_name = account_mapping.get(int(from_account_name), None)
 
+    # Fetch from account
     from_account_obj: Account = (
         session.query(Account)
         .filter(Account.name == from_account_name.upper(), Account.is_active == True)
         .first()
     )
 
+    # Validate from account and amount exceeds transfer amount
     if not from_account_obj:
         raise Exception(f"No active account found with name {from_account_name}!")
-
-    print("Enter to account number: ")
-    for i, account in account_mapping.items():
-        print(f"\n({i}) {account.name}")
-
-    to_account_name = input().strip()
-    if to_account_name.lower() in exit_keys:
-        return
-    to_account_name = account_mapping.get(int(to_account_name), None)
-
-    to_account_obj: Account = (
-        session.query(Account)
-        .filter(Account.name == to_account_name.upper(), Account.is_active == True)
-        .first()
-    )
-    if not to_account_obj:
-        raise Exception(f"No active account found with name {to_account_name}!")
-
     if amount_in_cents > from_account_obj.value_in_cents:
         raise Exception(
             f"{amount_in_cents} greater than accounts value {from_account_obj.value_in_cents}!"
         )
 
+    # User selects to account from list of active accounts
+    print("Enter to account number: ")
+    for i, account in account_mapping.items():
+        print(f"\n({i}) {account.name}")
+    to_account_name = input().strip()
+    if to_account_name.lower() in exit_keys:
+        return
+    to_account_name = account_mapping.get(int(to_account_name), None)
+
+    # Fetch to account
+    to_account_obj: Account = (
+        session.query(Account)
+        .filter(Account.name == to_account_name.upper(), Account.is_active == True)
+        .first()
+    )
+
+    # Validate to account
+    if not to_account_obj:
+        raise Exception(f"No active account found with name {to_account_name}!")
+
+    # Perform transfer
     to_account_obj.value_in_cents += amount_in_cents
     from_account_obj.value_in_cents -= amount_in_cents
     session.commit()
