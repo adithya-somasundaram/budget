ed5caa2e2297c3057893ea0d75356835a773a9e3
adithya-somasundaram <43027530+adithya-somasundaram@users.noreply.github.com>
2026-04-30 19:17:50 -0700

    Budget/bulk create budget categories v1 (#46)
    * bulk create budget
    
    * move create budget to infra and adjust readme

--- src/budget_categories/infra.py
+++ src/budget_categories/infra.py
@@ -1,6 +1,29 @@
 from src.budget_categories.model import BudgetCategory
 
 
+def create_budget_category(session, name: str, amount_in_cents=0) -> None:
+    """Creates new budget category with given name and amount_in_cents. Name must be unique among active budget categories."""
+    # check for dupes
+    dupe_check = (
+        session.query(BudgetCategory)
+        .filter(BudgetCategory.name == name.upper(), BudgetCategory.is_active == True)
+        .first()
+    )
+
+    if dupe_check:
+        raise Exception(
+            f"Budget with name {name} exists with value {dupe_check.amount_in_cents}"
+        )
+
+    budget_category = BudgetCategory(
+        name=name.upper(), is_active=True, amount_in_cents=amount_in_cents
+    )
+
+    session.add(budget_category)
+    session.commit()
+    print(f"Created budget category with name {name} and value {amount_in_cents} cents")
+
+
 def get_budget_category_mapping(session) -> dict[int, BudgetCategory]:
     """Returns mapping of integer to budget category object for all active budget categories. Good for user input."""
     active_budget_categories = _get_all_active_budget_categories(session)
--- src/budget_categories/services.py
+++ src/budget_categories/services.py
@@ -1,33 +1,32 @@
 from src.accounts.infra import get_liquid_total
 from src.budget_categories.model import BudgetCategory
-from src.helpers import cents_to_dollars_str
+from src.helpers import cents_to_dollars_str, exit_keys
+from src.budget_categories.infra import create_budget_category
 
 
-def create_budget_category(session, name: str, amount_in_cents=0) -> None:
-    """Creates new budget category with given name and amount_in_cents. Name must be unique among active budget categories."""
-    # check for dupes
-    dupe_check = (
-        session.query(BudgetCategory)
-        .filter(BudgetCategory.name == name.upper(), BudgetCategory.is_active == True)
-        .first()
+def bulk_create_budget_categories(session) -> None:
+    print(
+        "Lets create some budget categories! Enter 'quit' or 'exit' at any time to save and exit."
     )
 
-    if dupe_check:
-        print(f"Budget with name {name} exists with value {dupe_check.amount_in_cents}")
-        return
+    while True:
+        name = input("Enter budget category name: ").strip()
+        if name.lower() in exit_keys:
+            return
 
-    budget_category = BudgetCategory(
-        name=name.upper(), is_active=True, amount_in_cents=amount_in_cents
-    )
+        amount = input(
+            "Enter budget amount in cents, click 'Enter' to set to 0: "
+        ).strip()
+        if amount.lower() in exit_keys:
+            return
+        elif amount == "":
+            amount = 0
 
-    try:
-        session.add(budget_category)
-        session.commit()
-        print(
-            f"Created budget category with name {name} and value {amount_in_cents} cents"
-        )
-    except Exception as e:
-        print(f"Error creating budget category: {str(e)}")
+        try:
+            create_budget_category(session, name, int(amount))
+        except Exception as e:
+            print(f"Error creating budget category: {str(e)}")
+            session.rollback()
 
 
 def deactivate_budget_category(session, budget_category_name: str) -> None:
