5b1ea52bc0294d754379eb7fb3d11092fe75dfc6
Adithya <aditsom@gmail.com>
2026-04-30 18:02:20 -0700

    comments

--- src/budget_categories/infra.py
+++ src/budget_categories/infra.py
@@ -1,6 +1,15 @@
 from src.budget_categories.model import BudgetCategory
 
 
+def get_budget_category_mapping(session) -> dict[int, BudgetCategory]:
+    """Returns mapping of integer to budget category object for all active budget categories. Good for user input."""
+    active_budget_categories = _get_all_active_budget_categories(session)
+    return {
+        i: budget_category
+        for i, budget_category in enumerate(active_budget_categories, 1)
+    }
+
+
 def _get_all_active_budget_categories(session) -> list[BudgetCategory]:
     """Returns list of all active budget category names"""
     return (
@@ -9,12 +18,3 @@ def _get_all_active_budget_categories(session) -> list[BudgetCategory]:
         .order_by(BudgetCategory.name.asc())
         .all()
     )
-
-
-def get_budget_category_mapping(session) -> dict[int, BudgetCategory]:
-    """Returns mapping of budget category name to budget category object for all active budget categories"""
-    active_budget_categories = _get_all_active_budget_categories(session)
-    return {
-        i: budget_category
-        for i, budget_category in enumerate(active_budget_categories, 1)
-    }
