84e5d6cfd8eec72e9f387e49b490ddd0d6df3a89
Adithya <aditsom@gmail.com>
2026-06-06 18:36:10 -0700

    print

--- scripts.py
+++ scripts.py
@@ -6,3 +6,18 @@ from src.transactions.services import *
 
 app.app_context().push()
 db.create_all()
+
+print(
+    """
+Available functions:
+- bulk_create_accounts: create accounts
+- bulk_create_budget_categories: create budget categories
+- adjust_budget_category: adjust a budget category's amount
+- deactivate_budget_category: deactivate a budget category
+- bulk_create_transactions: create transactions
+- transfer_input: transfer between accounts
+- create_credit_payment: pay off a credit account
+- print_summary / print_liquid_summary: view account and budget summaries
+- view_all_transactions: view recorded transactions
+"""
+)
